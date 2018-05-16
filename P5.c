ca2b2c793bd370df631e79fa63f91d73bc1ae25b
preguicadeternome <38092859+preguicadeternome@users.noreply.github.com>
2018-05-16 17:44:50 -0300

    Add files via upload

---
+++ P5.c
@@ -0,0 +1,48 @@
+//Tales Oliveira Neves, 11721ECP010
+#include <stdio.h>
+
+void main ()
+{
+    int men, a;
+    int vint[64];
+    char vchar [255];
+    int* pint = (int *) vchar;
+    char* pchar = (char *) &vint;
+    printf("1- Codificar\n2- Descodificar\n");
+    scanf("%d", &men);
+    a =getchar();
+    switch(men)
+    {
+    case 1:
+        for(a=0;a<255;a++)
+        {
+            vchar[a]=0;
+        }
+        printf("Insira uma frase\n");
+        fgets(vchar,255,stdin);
+        for(a=0;vchar[a]!=0;a++)
+        {
+            if(vchar[a]==10){vchar[a]=0;}
+        }
+        for(a=0;*(pint+a)!=0;a++)
+        {
+            printf("%d ", *(pint+a));
+        }
+        break;
+    case 2:
+        printf("Insira um numero\n");
+        for(a=0;a<64;a++)
+        {
+            scanf("%d", &vint[a]);
+            if(getchar()=='\n')
+            {
+                break;
+            }
+        }
+        for (a=0;*(pchar+a)!=0;a++)
+        {
+        printf("%c", *(pchar+a));
+        }
+        break;
+    }
+}
